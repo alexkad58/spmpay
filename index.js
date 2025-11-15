b947bd57058dbf89a0598589cea11bc6f80d073f
alexkad58 <alexkad58@gmail.com>
2025-11-15 23:40:32 +0300

    fix

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get("/success", (req, res) => {
 app.post('/webhook', (req, res) => {
   const webhookData = req.body;
   console.log(webhookData)
-  const message = `*Оплата через SP Pay*\n\nплательщик \\- *${webhookData.payer}*\nтовар \\- *${webhookData.data}*\nсумма \\- *${webhookData.amount} АР*`
+  const message = `*Оплата через SP Pay*\n\nплательщик \\- *${webhookData.payer}*\nтовар \\- *${webhookData.data}*\nсумма \\- *${webhookData.amount} АР*`.replace('_', '\\_')
   sendMessage(message)
 
   res.status(200).send('Webhook received and processed successfully');
