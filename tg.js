89c1c64d74898e4e18e2e5ea438f9d7fe1c27917
alexkad58 <alexkad58@gmail.com>
2025-11-15 21:31:00 +0300

    export fix

--- tg.js
+++ tg.js
@@ -5,8 +5,6 @@ dotenv.config();
 const token = process.env.TG;
 const bot = new TelegramBot(token, {polling: true});
 
-const sendMessage = (text) => {
+export const sendMessage = (text) => {
     bot.sendMessage('-4774407794', text)
 }
-
-module.exports = { sendMessage }
