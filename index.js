288c1427915e157ff42def5cdc156d805cc30c91
alexkad58 <alexkad58@gmail.com>
2025-11-17 18:39:28 +0300

    fix

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ app.get("/success", (req, res) => {
 
 app.post('/webhook', (req, res) => {
   const webhookData = req.body;
+  console.log(webhookData, req)
   isValid = api.validateHash(webhookData, req.headers['X-Body-Hash'])
+  console.log(isValid)
   if (!isValid) return log(`Не валидный вебхук: ${JSON.stringify(webhookData)}`)
 
   const message = `*Оплата через SP Pay*\n\nплательщик - *${webhookData.payer}*\nтовар - *${webhookData.data}*\nсумма - *${webhookData.amount} АР*`
